946282ee036c01ac9419136ab703dbec21cebbc5
battlin <battlin6@gmail.com>
2021-03-09 03:21:46 +0800

    update 2

--- deque/DataForOJ/2.cpp
+++ deque/DataForOJ/2.cpp
@@ -10,16 +10,15 @@
 #include "class-integer.hpp"
 #include "class-matrix.hpp"
 #include "class-bint.hpp"
-#include "deque.hpp"
+#include "src.hpp"
 
-std::default_random_engine randnum(time(NULL));
-static const int MAX_N = 15000;
+std::default_random_engine randnum(time(NULL)+7);
+static const int MAX_N = 10007;
 
 template <typename Ans, typename Test>
 bool isEqual(Ans &ans, Test &test) {
     if (ans.size() != test.size())
         return false;
-
     if (ans.empty()) return true;
 
     for (int i = 0; i < ans.size(); i++) {
@@ -30,15 +29,12 @@ bool isEqual(Ans &ans, Test &test) {
         }
     }
 
-    if (ans.empty() != test.empty() || ans.front() != test.front() ||
-        ans.back() != test.back())
-        return false;
-
-    return true;
+    return !(ans.empty() != test.empty() || ans.front() != test.front() ||
+             ans.back() != test.back());
 }
 
 template <typename Ans, typename Test>
-void randnumFill(Ans &ans, Test &test, int n = 2e5) {
+void randnumFill(Ans &ans, Test &test, int n = 15000) {
     for (int i = 0; i < n; i++) {
         int x = randnum();
         if (randnum() % 2) {
@@ -50,6 +46,34 @@ void randnumFill(Ans &ans, Test &test, int n = 2e5) {
         }
     }
 }
+
+//for copy and clear test
+int ansCounter = 0, myCounter = 0, noUseCounter = 0;
+class DynamicType {
+public:
+    int *pct;
+    double *data;
+    DynamicType (int *p) : pct(p) , data(new double[2]) {
+        (*pct)++;
+    }
+    DynamicType (const DynamicType &other) : pct(other.pct), data(new double[2]) {
+        (*pct)++;
+    }
+    DynamicType &operator =(const DynamicType &other) {
+        if (this == &other) return *this;
+        (*pct)--;
+        pct = other.pct;
+        (*pct)++;
+        delete [] data;
+        data = new double[2];
+        return *this;
+    }
+    ~DynamicType() {
+        delete [] data;
+        (*pct)--;
+    }
+    bool operator != (const DynamicType &rhs) const { return false; }
+};
 
 bool pushTest() {
     std::deque<int> ans;
@@ -67,8 +91,8 @@ bool pushTest() {
 
     return isEqual(ans, deq);
 }
-
 bool popTest() {
+
     std::deque<long long> ans;
     sjtu::deque<long long> deq;
 
@@ -85,14 +109,14 @@ bool popTest() {
 
     return isEqual(ans, deq);
 }
-
 bool insertTest() {
+
     std::deque<int> ans, ans2, ans3;
     sjtu::deque<int> deq, deq2, deq3;
 
     for (int i = 0; i < 100; i++) {
         int x = randnum();
-        int pos = (ans.size() == 0 ? 0 : randnum() % ans.size());
+        int pos = (ans.empty() ? 0 : randnum() % ans.size());
 
         switch (randnum() % 2) {
             case 0: deq.insert(deq.begin() + pos, x);
@@ -122,7 +146,7 @@ bool iteratorTest() {
     auto myIter  = deq.begin() + deq.size() / 2;
 
     // iter++, iter--
-    for (int i = 0; i < MAX_N; i++) {
+    for (int i = 0; i < MAX_N / 2 ; i++) {
         switch(randnum() % 2) {
             case 0: ansIter++; myIter++;
                 break;
@@ -152,59 +176,21 @@ bool iteratorTest() {
             return false;
     }
 
-    // iter +=n, iter -= n, iter += -n, iter -= -n
-    int index = ans.size() / 2;
-    ansIter = ans.begin() + index;
-    myIter  = deq.begin() + index;
-    for (int i = 0; i < MAX_N * 2; i++) {
-        int offset = randnum() % (ans.size() / 2);
-        switch (randnum() % 4) {
-            case 0:  // iter += n
-                if (index + offset < ans.size()) {
-                    index += offset;
-                    ansIter += offset; myIter += offset;
-                }
-                break;
-            case 1: // iter -= n
-                if (index - offset >= 0) {
-                    index -= offset;
-                    ansIter -= offset; myIter -= offset;
-                }
-                break;
-            case 2:  // iter += -n
-                if (index - offset >= 0) {
-                    index += -offset;
-                    ansIter += -offset; myIter += -offset;
-                }
-                break;
-            case 3: // iter -= -n
-                if (index + offset < ans.size()) {
-                    index -= -offset;
-                    ansIter -= -offset; myIter -= -offset;
-                }
-                break;
-        }
-        if (*ansIter != *myIter)
-            return false;
-        *ansIter = *myIter = randnum();
-    }
-
     // traverse
     ansIter = ans.begin(); myIter = deq.begin();
     while (ansIter != ans.end()) {
         if (*ansIter++ != *myIter++)
             return false;
     }
+
     //  reversal traverse
     ansIter = ans.end() - 1; myIter = deq.end() - 1;
     while (ansIter != ans.begin()) {
         if (*ansIter-- != *myIter--)
             return false;
     }
-
     return *ansIter == *myIter;
 }
-
 bool eraseTest() {
     std::deque<int> ans;
     sjtu::deque<int> deq;
@@ -226,79 +212,7 @@ bool eraseTest() {
 
     return isEqual(ans, deq);
 }
-
-int ansCounter = 0, myCounter = 0, noUseCounter = 0;
-// you should construct and deconstruct this class correctly
-class DynamicType {
-public:
-    int *pct;
-    double *data;
-    DynamicType (int *p) : pct(p) , data(new double[2]) {
-        (*pct)++;
-    }
-    DynamicType (const DynamicType &other) : pct(other.pct), data(new double[2]) {
-        (*pct)++;
-    }
-    DynamicType &operator =(const DynamicType &other) {
-        if (this == &other) return *this;
-        (*pct)--;
-        pct = other.pct;
-        (*pct)++;
-        delete [] data;
-        data = new double[2];
-        return *this;
-    }
-    ~DynamicType() {
-        delete [] data;
-        (*pct)--;
-    }
-    bool operator != (const DynamicType &rhs) const { return false; }
-};
-
-bool copyAndClearTest() {
-    // you should call the constructor and deconstructor correctly
-    {
-        std::deque<DynamicType> ans;
-        sjtu::deque<DynamicType> deq, deq2 = deq, deq3(deq2);
-
-        // empty copy and clear
-        deq.clear(); deq2.clear();  deq3.clear();
-        deq = deq = deq; deq2 = deq3; deq3 = deq = deq3 = deq2;
-
-        for (int i = 0; i < MAX_N; i++) {
-            ans.push_back(DynamicType(&ansCounter));
-            deq.push_back(DynamicType(&myCounter));
-        }
-        if (myCounter != ansCounter) return false;
-
-        deq = deq = deq = deq;
-        if (myCounter != ansCounter) return false;
-
-        deq = deq2 = deq2 = deq;
-        if (myCounter != 2 * ansCounter) return false;
-        if (!isEqual(deq, deq2)) return false;
-
-        deq2 = deq = deq = deq2 = deq2 = deq3;
-        if (myCounter != 0) return false;
-        if (!isEqual(deq, deq2)) return false;
-
-        for (int i = 0; i < MAX_N; i++)
-            deq.push_back(DynamicType(&myCounter));
-
-        deq2 = deq3 = deq;
-        if (myCounter != 3 * ansCounter) return false;
-
-        deq.clear();
-        sjtu::deque<DynamicType> deq4(deq), deq5(deq2);
-        if (myCounter != 3 * ansCounter) return false;
-        if (isEqual(deq, deq2)) return false;
-    }
-
-    return myCounter == ansCounter;
-}
-
 bool memoryTest() {
-    // you should call the constructor and deconstructor correctly
     std::deque<DynamicType> ans;
     sjtu::deque<DynamicType> deq;
 
@@ -332,30 +246,7 @@ bool memoryTest() {
         if (ansCounter != myCounter)
             return false;
     }
-
     return isEqual(ans, deq);
-}
-
-bool exceptionTest() {
-    sjtu::deque<int> deq, deq2;
-    int ct = 0;
-
-    try { deq.front(); } catch (...) { ct++; }
-    try { deq.back(); }  catch (...) { ct++; }
-    try { deq[0]; }      catch (...) { ct++; }
-
-    deq.push_back(1); deq.push_back(2);
-    deq2.push_back(1); deq2.push_back(2);
-
-    try { deq.end() - deq.begin(); }  catch (...) { ct--; }
-    try { deq2.end() - deq.begin(); } catch (...) { ct++; }
-
-    try { deq2[-1]; }    catch (...) { ct++; }
-    try { deq2[2]; }     catch (...) { ct++; }
-    try { deq2.at(-1); } catch (...) { ct++; }
-    try { deq2.at(1); }  catch (...) { ct--; }
-
-    return ct == 7;
 }
 
 template <typename T>
@@ -427,7 +318,7 @@ bool nomercyTest() {
     {
         std::deque<int> ans;
         sjtu::deque<int> deq;
-        randnumFill(ans, deq, 100000);
+        randnumFill(ans, deq, 50000);
         if (!dfs(19960904, ans, deq))
             return false;
 
@@ -446,31 +337,34 @@ bool nomercyTest() {
 int main() {
     bool (*testFunc[])()= {
             pushTest, popTest, insertTest, iteratorTest,
-            eraseTest, copyAndClearTest, memoryTest,
+            eraseTest, memoryTest,
             nomercyTest,
     };
 
     const char *testMessage[] = {
             "Testing push...", "Testing pop...", "Testing insert...", "Testing iterator...",
-            "Testing erase...", "Testing copy and clear...", "Testing memory...",
-            "Final test without mercy...",
+            "Testing erase...", "Testing memory...",
+            "Final test...",
     };
 
     bool error = false;
     for (int i = 0; i < sizeof(testFunc) / sizeof(testFunc[0]); i++) {
-        printf("%-40s", testMessage[i]);
+        //printf("%-40s", testMessage[i]);
         if (testFunc[i]())
-            printf("Passed\n");
+            //printf("Passed\n");
+            continue;
         else {
             error = true;
-            printf("Failed !!!\n");
+            //printf("Failed !!!\n");
         }
     }
 
     if (error)
-        printf("\nUnfortunately, you failed in this test\n\a");
+        //printf("\nUnfortunately, you failed in this test\n\a");
+        std::cout<<0<<std::endl;
     else
-        printf("\nCongratulations, your deque passed all the tests!\n");
+        //printf("\nCongratulations, your deque passed all the tests!\n");
+        std::cout<<1<<std::endl;
 
     return 0;
 }
